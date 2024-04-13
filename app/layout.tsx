cb3c695ed10c734826ea5302ef3fe26affd51862
rii98 <136151503+rii98@users.noreply.github.com>
2024-04-13 17:24:37 +0545

    Test1

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}hi</body>
+      <body className={inter.className}>{children}hitest</body>
     </html>
   );
 }
