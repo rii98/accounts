ab438b766d463ff9838861fc2657ddbfcd3120eb
rii98 <136151503+rii98@users.noreply.github.com>
2024-04-13 17:36:00 +0545

    Update layout.tsx aa

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}hitest from the web.</body>
+      <body className={inter.className}>{children}hitest from the web aa.</body>
     </html>
   );
 }
